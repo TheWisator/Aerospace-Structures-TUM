b98e5810b73080087abd6ca0d7893d60921fcd6b
TheWisator <thilo.witzel@web.de>
2023-05-11 21:14:44 +0200

    Update Exercise 3

--- exercise_sheet_3.jl
+++ exercise_sheet_3.jl
@@ -1,33 +1,43 @@
-using Plots, LinearAlgebra, Unitful
+using Plots, LinearAlgebra, Unitful, DataFrames
 #plotly()
 
 ## Material Definition of Ti6Al4V
-E_ti = 114000
+E_ti = 114_000
 ν_ti = 0.33
 R_p02_ti = 830
+ρ_ti = 4_430
+
+## Material Definition of Al 7075
+E_al = 70_000
+R_p02_al = 460
+ρ_al = 2_800
+
+## Material Definition of stainless steel
+E_ss = 210_000
+R_p02_ss = 960
+ρ_ss = 7_800
+
+## Material Definition of CFRP
+E_cf = 90_000
+R_p02_cf = 800
+ρ_cf = 1_500
 
 ## Definition Case 1: Orbiter / B747
-A_c1 = 621.1
-D_c1 = 28.1
 L_1 = 1796.6
 k_c1 = (E_ti * A_c1 / L_1)
-Fx_c1 = 90300
-Fy_c1 = 154800
+Fx_c1 = 90_300
+Fy_c1 = 154_800
 ϕ_c1 = 52.8 * π / 180
 
 ## Definition Case 2: Orbiter / B747
-A_c2 = 2410.9
-D_c2 = 55.4
 L_2 = 1796.6
 k_c2 = (E_ti * A_c2 / L_2)
-Fx_c2 = 404800
-Fy_c2 = 529300
+Fx_c2 = 404_800
+Fy_c2 = 529_300
 ϕ_c2 = 52.8 * π / 180
 
 
 
-
-## Case 1
 # define Master Stiffness Matrix
 K(p1, p2, k1, k2) = ustrip.([   k1 .* cos.(p1).^2 -k1 .* sin.(p1) .* cos.(p1) -k1 .* cos.(p1).^2 k1 .* sin.(p1) .* cos.(p1) 0 0;
                 -k1 .* sin.(p1) .* cos.(p1) k1 .* sin.(p1).^2 k1 .* sin.(p1) .* cos.(p1) -k1 .* sin.(p1).^2 0 0;
@@ -52,6 +62,7 @@ u(u3, u4)   =  [0;
                 u4;
                 0;
                 0]
+
 
 function solve(ϕ1, ϕ2, Fx, Fy, k1, k2, A1, A2)
     # 6x6 stiffness matrix according to sheet
@@ -79,10 +90,10 @@ function solve(ϕ1, ϕ2, Fx, Fy, k1, k2, A1, A2)
     return (u_free, F_r, σ_r)
 end
 
-function iterate(ϕ1, ϕ2, Fx, Fy, E)
+function iterate_for_stress(ϕ1, ϕ2, Fx, Fy, E, ρ, RF, Rp02)
     A_iter = 100
     k_iter = E * A_iter / L_1
-    σ_req = R_p02_ti / 3
+    σ_req = Rp02 / RF
     solved_iter = solve(ϕ1, ϕ2, Fx, Fy, k_iter, k_iter, A_iter, A_iter)
     σ_cache = maximum(solved_iter[3])
     residual = σ_req - σ_cache
@@ -96,124 +107,98 @@ function iterate(ϕ1, ϕ2, Fx, Fy, E)
         σ_cache = maximum(solved_iter[3])
         residual = σ_req - σ_cache
     end
-    return A_iter
+    
+    # results of optimal area and diameter assuming a circular rod
+    A_opt = A_iter
+    D_opt = sqrt(4A_opt/π)
+    m_opt = 2 * (D_opt/1000)^2 * π/4 * L_1/1000 * ρ
+    return (A_opt, D_opt, m_opt)
+end
+
+function iterate_for_deformation(ϕ1, ϕ2, Fx, Fy, E, ρ, u_max)
+    A_iter = 100
+    k_iter = E * A_iter / L_1
+    u_req = u_max
+    solved_iter = solve(ϕ1, ϕ2, Fx, Fy, k_iter, k_iter, A_iter, A_iter)
+    u_cache = sqrt(solved_iter[1][1]^2 + solved_iter[1][2]^2)
+    residual = u_req - u_cache
+
+
+
+    while abs(residual) > 0.001
+        A_iter = A_iter * (1 - residual / u_req)
+        k_iter = E * A_iter / L_1
+        solved_iter = solve(ϕ1, ϕ2, Fx, Fy, k_iter, k_iter, A_iter, A_iter)
+        u_cache = sqrt(solved_iter[1][1]^2 + solved_iter[1][2]^2)
+        residual = u_req - u_cache
+    end
+    
+    # results of optimal area and diameter assuming a circular rod
+    A_opt = A_iter
+    D_opt = sqrt(4A_opt/π)
+    m_opt = 2 * (D_opt/1000)^2 * π/4 * L_1/1000 * ρ
+    return (A_opt, D_opt, m_opt)
 end
 
 
+function iterate(ϕ1, ϕ2, Fx, Fy, E, ρ, RF_stress, Rp02, u_max)
+    result_stress = iterate_for_stress(ϕ1, ϕ2, Fx, Fy, E, ρ, RF_stress, Rp02)
+    result_deformation = iterate_for_deformation(ϕ1, ϕ2, Fx, Fy, E, ρ, u_max)
+    if result_deformation[3] > result_stress[3]
+        println("Deformation is dimensioning")
+        return result_deformation
+    else
+        println("Stress is dimensioning")
+        return result_stress
+    end
+end
+
+## Design parameters
+RF_min = 3
+u_max = 3
 
 ## Case 1
 solved = solve(ϕ_c1,ϕ_c1, Fx_c1, Fy_c1, k_c1, k_c1, A_c1, A_c1)
 
 println("CASE 1: B747")
-println("Solved Area Case 1 [mm²]:")
-println(iterate(ϕ_c1,ϕ_c1, Fx_c1, Fy_c1, E_ti))
-println("Displacement Vector:")
-println(u(solved[1][1], solved[1][2]))
-println("Force Vector:")
-println(solved[2])
-println("Stresses")
-println(solved[3])
+println("Solved Case 1 (Area [mm²], Diameter [mm], Mass [kg]):")
+println(iterate(ϕ_c1,ϕ_c1, Fx_c1, Fy_c1, E_ti, ρ_ti, RF_min, R_p02_ti, u_max))
+println("----")
 
 ## Case 2
 solved = solve(ϕ_c1,ϕ_c1, Fx_c2, Fy_c2, k_c2, k_c2, A_c2, A_c2)
 
 println("CASE 2: Tank")
-println("Solved Area Case 2 [mm²]:")
-println(iterate(ϕ_c2, ϕ_c2, Fx_c2, Fy_c2, E_ti))
-println("Displacement Vector:")
-println(u(solved[1][1], solved[1][2]))
-println("Force Vector:")
-println(solved[2])
-println("Stresses")
-println(solved[3])
+println("Solved Area Case 2 (Area [mm²], Diameter [mm], Mass [kg]):")
+println(iterate(ϕ_c2, ϕ_c2, Fx_c2, Fy_c2, E_ti, ρ_ti, RF_min, R_p02_ti, u_max))
+println("----")
 
+println("Because Case 2 is dimensioning, the following calculations will only be performed for this case")
+println("")
+print("Ti6Al4V: ")
+ret_ti = (iterate(ϕ_c2, ϕ_c2, Fx_c2, Fy_c2, E_ti, ρ_ti, RF_min, R_p02_ti, u_max))
 
-#= TASK 4
-ϕ_series = range(0.1, deg2rad(89), length=100)
+print("Alu 7075: ")
+ret_al = (iterate(ϕ_c2, ϕ_c2, Fx_c2, Fy_c2, E_al, ρ_al, RF_min, R_p02_al, u_max))
 
-# result vactors
-u_y = []
-detK = []
-eigenvalK = []
-eigenvecK = []
+print("Stainless Steel: ")
+ret_ss = (iterate(ϕ_c2, ϕ_c2, Fx_c2, Fy_c2, E_ss, ρ_ss, RF_min, R_p02_ss, u_max))
 
-for ϕ in ϕ_series
-    solved_cache = solve_task_three(ϕ, ϕ, Fx_c1, Fy_c1)
-    K_matrix = K(ϕ, ϕ)
-    append!(u_y, solved_cache[1][2])
-    append!(detK, det(K_matrix))
-    append!(eigenvalK, [round.(eigvals(K_matrix), digits=2)])
-    append!(eigenvecK, [round.(eigvecs(K_matrix), digits=2)])
-end
+print("CFRP: ")
+ret_cf = (iterate(ϕ_c2, ϕ_c2, Fx_c2, Fy_c2, E_cf, ρ_cf, RF_min, R_p02_cf, u_max))
 
-p1 = plot(rad2deg.(ϕ_series), u_y)
+names = ["Ti6Al4V"; "AL-7075"; "Stainless Steel"; "CFRP"]
+areas = [ret_ti[1]; ret_al[1]; ret_ss[1]; ret_cf[1]]
+diams = [ret_ti[2]; ret_al[2]; ret_ss[2]; ret_cf[2]]
+masses = [ret_ti[3]; ret_al[3]; ret_ss[3]; ret_cf[3]]
 
-xlabel!(p1, "ϕ [degrees]")
-ylabel!(p1, "u_y")
-#=println("Eigenvalues of K")
-display(eigenvalK)
-println("Eigenvectors of K")
-display(eigenvecK)=#
+result = DataFrame()
+result.material = names
+result."Area [mm]" = areas
+result."Diameter [mm]" = diams
+result."Mass [kg]" = masses
+display(result)
 
-display(p1) 
-
-## ADD-ON: Alternative way to find K
-function solve_task_alternatively(ϕ1, ϕ2, Fx, Fy)
-    # rotation matrix
-    T(α) = [    cos(α) -sin(α) 0 0;
-                sin(α) cos(α) 0 0;
-                0 0 cos(α) -sin(α);
-                0 0 sin(α) cos(α)]
-
-    ## Element 1
-    K_1 = k1 * [1 0 -1 0;
-                0 0 0 0;
-                -1 0 1 0;
-                0 0 0 0;] 
-    T1 = T(ϕ1)
-    # 4x4 in global sytem
-    K_1e = transpose(T1) * K_1 * T1
-    # extend to 6x6 in global system
-    K_1e = [    K_1e[1,1] K_1e[1,2] K_1e[1,3] K_1e[1,4] 0 0;
-                K_1e[2,1] K_1e[2,2] K_1e[2,3] K_1e[2,4] 0 0;
-                K_1e[3,1] K_1e[3,2] K_1e[3,3] K_1e[3,4] 0 0;
-                K_1e[4,1] K_1e[4,2] K_1e[4,3] K_1e[4,4] 0 0;
-                0 0 0 0 0 0;
-                0 0 0 0 0 0] 
-
-    ## Element 2
-    K_2 = k2 * [1 0 -1 0;
-                0 0 0 0;
-                -1 0 1 0;
-                0 0 0 0;]  
-    T2 = T(-ϕ2)
-    #4x4 in global system
-    K_2e = transpose(T2) * K_2 * T2
-    # extend to 6x6 in global system
-    K_2e = [    0 0 0 0 0 0;
-                0 0 0 0 0 0;
-                0 0 K_2e[1,1] K_2e[1,2] K_2e[1,3] K_2e[1,4];
-                0 0 K_2e[2,1] K_2e[2,2] K_2e[2,3] K_2e[2,4];
-                0 0 K_2e[3,1] K_2e[3,2] K_2e[3,3] K_2e[3,4];
-                0 0 K_2e[4,1] K_2e[4,2] K_2e[4,3] K_2e[4,4]]
-
-    ## Global Evaluation
-    # global 6x6 stiffness matrix
-    K_ges = K_1e + K_2e
-    # 6x1 displacement vector
-    F_vec = F(Fx, Fy)
-    # solve stiffness equation for two free displacements
-    u_free = inv(K_ges[3:4, 3:4]) * F_vec[3:4]
-    # calculate reaction Forces
-    F_r = K_ges * u(u_free[1], u_free[2])
-
-    return (u_free, F_r)
-end
-
-solved = solve_task_alternatively(π/4,π/4,1,-0.8)
-
-println("Displacement Vector alternatively:")
-println(u(solved[1][1], solved[1][2]))
-println("Force Vector alternatively:")
-println(solved[2])
-=#
+# A = result."Area [mm]"[1]
+# k = E_ti * A / L_2
+# println(solve(ϕ_c2, ϕ_c2, Fx_c2, Fy_c2, k, k, A, A))
